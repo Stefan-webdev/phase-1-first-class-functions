c81e03ce6fd778b0aa252305751de5b0a2f577eb
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-07-15 13:56:15 +0930

    Lab Complete

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function receivesAFunction(callback){
+  return callback()
+
+}
+
+
+function returnsANamedFunction() {
+  function namedFunction() {
+    return "I am a named function";
+  }
+  return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    return "I am an anonymous function";
+  };
+}
